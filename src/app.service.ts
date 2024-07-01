6a560928893e96359dd889f1f828ffc2b75ac73d
cfcife <aboludepeter@gmail.com>
2024-07-01 03:07:50 +0100

    refactor: Update app.service.ts to use X-Vercel-IP-City header for geolocation

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 type returnBody = {
   client_ip?: string;
@@ -23,38 +24,21 @@ export class AppService {
     const client_name = name.charAt(0).toUpperCase() + name.slice(1);
 
     // Get the client's IP address
-    // // const ip = await ipAddress(request);
-    // console.log(request.headers['x-forwarded-for']);
-    // const ip = '8.8.8.8';
-    // const { city } = geolocation(request);
-    // console.log(
-    //   new Response(city, { headers: { 'content-type': 'text/html' } }),
-    // );
+    const ip = request.headers['x-forwarded-for'];
+
     // // Get the client's location
-    // const client_location_response = await axios.get(
-    //   `https://api.ipdata.co/${ip}?api-key=${this.ipdata_apikey}`,
-    // );
+    const city = request.headers['x-vercel-ip-city'];
 
-    // // const { city } = client_location_response.data;
+    const client_temp_response = await axios.get(
+      `http://api.weatherapi.com/v1/current.json?key=${this.weather_apikey}&q=${city}`,
+    );
 
-    const city = request.headers.get('x-vercel-ip-city');
-    console.log(city);
-    // const ip = request.headers['x-forwarded-for'];
-    // console.log(ip);
-
-    // const client_temp_response = await axios.get(
-    //   `http://api.weatherapi.com/v1/current.json?key=${this.weather_apikey}&q=${city}`,
-    // );
-
-    // const client_temp = client_temp_response.data.current.temp_c;
+    const client_temp = client_temp_response.data.current.temp_c;
 
     return {
-      client_ip: '8.8.8.8',
-      location: 'a string',
-      greeting: 'a string',
-      // client_ip: ip,
-      // location: city,
-      // greeting: `Hello, ${client_name}!, the temperature is ${client_temp} degrees Celsius in ${city}`,
+      client_ip: ip,
+      location: city,
+      greeting: `Hello, ${client_name}!, the temperature is ${client_temp} degrees Celsius in ${city}`,
     };
   }
 }
