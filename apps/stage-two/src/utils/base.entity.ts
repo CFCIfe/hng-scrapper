1969963ffd512e154559173d0a6c97e1f2f52b9f
cfcife <aboludepeter@gmail.com>
2024-07-07 22:23:53 +0100

    refactor: Remove unused files and code
    This commit removes several unused files and code from the repository. The deleted files include setup.ts, constants.ts, tsconfig.app.json, base.entity.ts, authenticated-request.interface.ts, jest-e2e.json, global-setup.ts, global-teardown.ts, helperFunctions.ts, and auth.module.ts. These files are no longer needed and their removal helps to declutter the codebase and improve maintainability.

--- apps/stage-two/src/utils/base.entity.ts
+++
@@ -1,8 +0,0 @@
-import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
-
-export abstract class Base {
-  @CreateDateColumn({ type: 'timestamp' })
-  createdAt: Date;
-  @UpdateDateColumn({ type: 'timestamp' })
-  updatedAt: Date;
-}
